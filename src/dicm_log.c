f2c51eb8b99f7d9a8b59086366f362b0a65a729f
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-12-08 09:46:57 +0100

    add logging

---
+++ src/dicm_log.c
@@ -0,0 +1,49 @@
+#include "dicm_log.h"
+
+#include <stdarg.h>    /* va_list */
+#include <stdatomic.h> /* _Atomic */
+#include <stdio.h>     /* fprintf */
+#include <string.h>    /* strlen */
+
+struct logging {
+  void (*fp_msg)(int, const char *) DICM_NONNULL;
+};
+
+static void _default_log_msg(int, const char *) DICM_NONNULL;
+
+static _Atomic struct logging global_log = {
+    /* log interface */
+    .fp_msg = _default_log_msg};
+
+static const char *log_level_str[] = {"trace", "debug", "info",
+                                      "warn",  "error", "fatal"};
+
+void _default_log_msg(int log_level, const char *msg) {
+  assert(log_level >= LOG_TRACE && log_level <= LOG_FATAL);
+  assert(msg);
+  assert(strlen(msg) < 512);
+  fprintf(stderr, "%s: %s\n", log_level_str[log_level], msg);
+}
+
+void _log_msg(enum log_level_type log_level, const char *fmt, ...) {
+  char buffer[512];
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(buffer, sizeof buffer, fmt, ap);
+  va_end(ap);
+#if 0
+  global_log.fp_msg(log_level, buffer);
+#else
+  struct logging tmp = atomic_load_explicit(&global_log, memory_order_relaxed);
+  tmp.fp_msg(log_level, buffer);
+#endif
+}
+
+void dicm_configure_log_msg(void (*fp_msg)(int, const char *)) {
+#if 0
+  global_log.fp_msg = fp_msg;
+#else
+  struct logging tmp = {.fp_msg = fp_msg};
+  atomic_store_explicit(&global_log, tmp, memory_order_relaxed);
+#endif
+}
